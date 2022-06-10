539eac85293aea30605e9830e60a34189ee215bb
Ethan Gutierrez <ethanlizzard1123@gmail.com>
2022-06-10 17:20:54 -0500

    bug with the while loops

--- resources/createBoard.js
+++ resources/createBoard.js
@@ -1,5 +1,5 @@
 const board = document.getElementById("board");
-const boardLayout = [
+let boardLayout = [
     [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
     [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
     [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
@@ -26,6 +26,33 @@ const boardLayout = [
     [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
     [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
 ];
+let boardLetters = [
+    ["a", "b", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "c", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+];
 //creates a text tile
 function creatTile(x, y, black) {
     const tile = document.createElement("div");
@@ -38,22 +65,110 @@ function creatTile(x, y, black) {
 function createBoard() {
     for (let i = 0; i < boardLayout.length; i++) {
         for (let o = 0; o < boardLayout[i].length; o++) {
-            creatTile(i, o, boardLayout[i][o]);
+            creatTile(i, o, boardLayout[o][i]);
         }
     }
 }
 
-//dev tools
+function fillBoard() {
+    const nodes = board.children;
+    let index = 0;
+    for (let i = 0; i < boardLetters.length; i++) {
+        for (let o = 0; o < boardLetters[i].length; o++) {
+            nodes[index].innerHTML = boardLetters[o][i];
+            index++;
+        }
+    }
+}
+
+
+
+/*
+game logic 
+*/
+let currentFocus = null;
+
+function clearAll() {
+    const nodes = board.children;
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].style.outlineColor = "gray";
+    }
+}
+
+function getStartIndex(x, y) {
+    return x * boardLayout.length;
+}
+
+function getIndex(x, y) {
+    return x * boardLayout.length + y;
+}
+
+function highlight(row) {
+    const nodes = board.children;
+    const Xindex = parseInt(currentFocus.style.left) / 30;
+    const Yindex = parseInt(currentFocus.style.top) / 30;
+    let first = true;
+    const startIndex = getIndex(Xindex, Yindex);
+    const highlightColor = "#ff0037";
+    if (row) {
+
+    } else {
+        let i = 0;
+        while (boardLayout[Yindex + i][Xindex]) {
+            if ((Yindex + i) > 24) break;
+            console.log(i);
+            console.log(Yindex + i);
+            console.log(startIndex + i);
+            nodes[startIndex + i].style.outlineColor = highlightColor;
+            i++;
+        }
+        i = 0;
+        while (boardLayout[Yindex - i][Xindex]) {
+            if ((Yindex - i) < 0) break;
+            nodes[startIndex - i].style.outlineColor = highlightColor;
+            i++;
+        }
+    }
+}
+
+function input(e) {
+    clearAll();
+    if (currentFocus != null) {
+        currentFocus.style.backgroundColor = "white";
+    }
+    if (currentFocus === e.target) {
+        currentFocus = null;
+        return;
+    }
+    if (e.target.getAttribute("class") == "edge") {
+        return;
+    }
+    currentFocus = e.target;
+    currentFocus.style.backgroundColor = "gray";
+    highlight(false);
+}
+
+/*
+
+
+        DEV TOOLS
+
+*/
 let inDev = false;
 
 //swaps between tile and edge
 function swap(e) {
     e = e.target;
+    const Xindex = parseInt(e.style.left) / 30;
+    const Yindex = parseInt(e.style.top) / 30;
     if (e.getAttribute("class") == "edge") {
         e.setAttribute("class", "tiles");
+        boardLayout[Yindex][Xindex] = true;
         return;
     }
     e.setAttribute("class", "edge");
+    boardLayout[Yindex][Xindex] = false;
+
 }
 
 //adds event listeners to board
@@ -72,16 +187,43 @@ function removeSwap() {
     }
 }
 
+//adds event listeners to board for the game element
+function addGame() {
+    const nodes = board.children;
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].addEventListener("click", input, false);
+    }
+}
+
+//removes swapping listeners from board for the game element
+function removeGame() {
+    const nodes = board.children;
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].removeEventListener("click", input, false);
+    }
+}
+
 //toggle dev mode
 document.getElementById("makeBoard").onclick = function() {
     inDev = !inDev;
     if (inDev) {
         document.getElementsByTagName("body")[0].style.backgroundColor = "blue";
         addSwap();
+        removeGame();
     } else {
         document.getElementsByTagName("body")[0].style.backgroundColor = "rgb(70, 138, 194)";
         removeSwap();
+        addGame();
     }
 };
 
+//get board layout
+document.getElementById("getBoard").onclick = function() {
+    console.log(boardLayout);
+}
+
 createBoard();
+fillBoard();
+addGame();
+addGame();
+addGame();
